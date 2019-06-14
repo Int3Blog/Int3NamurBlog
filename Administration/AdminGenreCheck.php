ca2a72445f7987a7b2725d8ec1840954a974a4c0
Joël D'henau <joel.dhenau@bstorm.be>
2019-06-14 10:44:11 +0200

    Ajout adminGenre

---
+++ Administration/AdminGenreCheck.php
@@ -0,0 +1,18 @@
+<?php
+include '../Communs/fonctionsDAL.php';
+
+    if(isset($_POST['Genre']))
+    {
+     if($_POST['Genre']==''){
+         $Genre =1;
+     }
+     else
+     {
+         $Genre = $_POST['Genre'];
+     }
+       
+     $bdd=connexionBDD();
+     ajoutGenre($Genre,$bdd);
+
+    }
+    header("location:adminGenre.php");
---
+++ Administration/admingenre.php
@@ -0,0 +1,40 @@
+<?php 
+include ("../Communs/fonctionsDAL.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Genre</title>
+	<link href="css.css" rel="stylesheet">
+</head>
+<body>
+	
+
+	<form action="AdminGenreCheck.php" method="POST" id="postForm">
+		<p><label for="genre"><strong>Genre</strong> :  </label><input type="text" name="Genre" id="Genre" size="64"/></p>
+	</form>
+	<p><input type="submit" form="postForm" value="Ajouter"/> </p>
+
+	<table>
+		<thead>
+			<th> ID </th>
+			<th> Genre </th>
+		</thead>
+		<tbody>
+		<?php
+			$bdd=connexionBDD();
+			$tabGenres=extraitListeGenres($bdd);
+			
+			for($i=0; $i < count($tabGenres); $i++)
+			{
+				echo "<tr>";
+				echo "<td>".$tabGenres[$i]["id"]."</td>";
+				echo "<td>".$tabGenres[$i]["genre"]."</td>";
+				echo "</tr>";
+			}
+		?>
+		</tbody>
+	</table>
+
+</body>
+</html>
